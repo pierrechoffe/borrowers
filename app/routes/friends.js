fe56189f6dbd717e4b8535e6f420e66e96d91446
pierre choffe <pc@orchestredeschampselysees.com>
2014-09-18 00:33:14 +0200

    second commit deleting friends

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -2,15 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	actions: {
-      save: function() {
-        console.log('save action bubbled to friends route');
- 
-        return true;
-      },
-      cancel: function() {
-        console.log('cancel action bubbled to friends route');
- 
-       return true;
-     }
-   }
+    save: function() {
+      console.log('save action bubbled to friends route');
+
+      return true;
+    },
+    cancel: function() {
+      console.log('cancel action bubbled to friends route');
+
+      return true;
+    },
+    delete: function(friend) {
+      friend.destroyRecord();
+      this.transitionTo('friends.index');
+    }
+  }
 });
